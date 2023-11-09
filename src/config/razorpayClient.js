310148cca8e8854a9f5da8e18364beeb1ec0b2b3
leninm <leninm@lecturenotes.in>
2023-11-08 21:23:01 -0500

    initial commit

---
+++ src/config/razorpayClient.js
@@ -0,0 +1,12 @@
+const Razorpay = require('razorpay');
+const API_KEY=process.env.API_KEY
+const API_SECRET=process.env.API_SECRET
+
+
+const razorpay = new Razorpay({
+    key_id: API_KEY,
+    key_secret: API_SECRET,
+  });
+
+
+  module.exports=razorpay;
